8c4ff9ddae44ea147564fc3efefcf4e1b4454c6a
Sanjay Idpuganti <sanjay11@outlook.com>
2019-07-28 23:35:07 +0530

    Moved the color responsibility to Record Component

--- src/app/record/record.component.ts
+++ src/app/record/record.component.ts
@@ -1,24 +1,43 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { IRecord } from '../models/record';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy
+} from "@angular/core";
+import { IRecord } from "../models/record";
 
 @Component({
-  selector: 'record',
-  templateUrl: './record.component.html',
-  styleUrls: ['./record.component.css']
+  selector: "record",
+  templateUrl: "./record.component.html",
+  styleUrls: ["./record.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecordComponent implements OnInit {
-  @Input('data') record: IRecord;
-  @Input('color') color: string;
-  @Output('delete') deleteEventEmitter = new EventEmitter<IRecord>();
-  @Output('addSimilar') addSimilarEventEmitter = new EventEmitter<IRecord>();
+  @Input("data") record: IRecord;
+  // @Input("color") color: string;
+  @Output("delete") deleteEventEmitter = new EventEmitter<IRecord>();
+  @Output("addSimilar") addSimilarEventEmitter = new EventEmitter<IRecord>();
 
   isFocused = false;
 
   showTags = true;
 
+  color: string;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.color =
+      this.record.type === 2
+        ? this.record.value > 100
+          ? "red"
+          : this.record.value > 70
+          ? "orange"
+          : "green"
+        : "black";
+  }
 
   public onDeleteButtonClick() {
     this.deleteEventEmitter.emit(this.record);
